3918852db9b163aa6f87782ea71413154dce0f2b
Thomas Schoffelen <thomas@schof.co>
2019-08-30 12:42:07 +0200

    Cleanup.

--- components/layout.js
+++ components/layout.js
@@ -12,7 +12,7 @@ export default ({ children, title = 'Thomas Schoffelen' }) => (
       <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32"/>
 
       <link href='https://rsms.me/inter/inter.css' rel='stylesheet'/>
-      <link href='/styles.css' rel='stylesheet'/>
+      <link href='/styles.css?v=2' rel='stylesheet'/>
 
       <meta name="google-site-verification" content="PfK2tt-swzI7S9DjGXMWXb6BEo09M6ATCR87bR5HqQE"/>
       <meta name="theme-color" content="#000000"/>
