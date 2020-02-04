9754d6bca4489efd25e60b69bf4d9d68c90e4b22
Thomas Schoffelen <thomas@schof.co>
2020-02-04 09:40:50 +0000

    Remove Gatsby version string.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-json`,
+    `gatsby-plugin-remove-generator`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
