55bbeeb1e823310629466bfc75f820c5fcdee0ea
Thomas Schoffelen <thomas@schof.co>
2020-02-05 22:30:48 +0000

    Some small performance improvements.

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,9 +1,9 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 
-function SEO({ description, lang, meta, title }) {
+function SEO ({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -16,14 +16,14 @@ function SEO({ description, lang, meta, title }) {
         }
       }
     `
-  )
+  );
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || site.siteMetadata.description;
 
   return (
     <Helmet
       htmlAttributes={{
-        lang,
+        lang
       }}
       title={
         title
@@ -33,44 +33,44 @@ function SEO({ description, lang, meta, title }) {
       meta={[
         {
           name: `viewport`,
-          content: `width=device-width, initial-scale=1.0, shrink-to-fit=no, minimum-scale=1.0`,
+          content: `width=device-width, initial-scale=1.0, shrink-to-fit=no, minimum-scale=1.0`
         },
         {
           name: `google-site-verification`,
-          content: `PfK2tt-swzI7S9DjGXMWXb6BEo09M6ATCR87bR5HqQE`,
+          content: `PfK2tt-swzI7S9DjGXMWXb6BEo09M6ATCR87bR5HqQE`
         },
         {
           name: `description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:title`,
-          content: title,
+          content: title
         },
         {
           property: `og:description`,
-          content: metaDescription,
+          content: metaDescription
         },
         {
           property: `og:type`,
-          content: `website`,
+          content: `website`
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary`
         },
         {
           name: `twitter:creator`,
-          content: site.siteMetadata.author,
+          content: site.siteMetadata.author
         },
         {
           name: `twitter:title`,
-          content: title,
+          content: title
         },
         {
           name: `twitter:description`,
-          content: metaDescription,
-        },
+          content: metaDescription
+        }
       ].concat(meta)}
     >
       <script>{`/*
@@ -101,11 +101,12 @@ function SEO({ description, lang, meta, title }) {
 
 
 */`}</script>
-      <link rel="canonical" href="https://schof.co" />
-      <link rel="me" href="https://twitter.com/tschoffelen" />
-      <link rel="me" href="https://instagram.com/tschoffelen" />
-      <link rel="me" href="https://github.com/tschoffelen" />
-      <link rel="pgpkey" href="/pgp.txt" />
+      <link rel="dns-prefetch" href="https://www.google-analytics.com"/>
+      <link rel="canonical" href="https://schof.co"/>
+      <link rel="me" href="https://twitter.com/tschoffelen"/>
+      <link rel="me" href="https://instagram.com/tschoffelen"/>
+      <link rel="me" href="https://github.com/tschoffelen"/>
+      <link rel="pgpkey" href="/pgp.txt"/>
       <script type="application/ld+json">{`
 {
   "@context": "http://schema.org",
@@ -147,20 +148,20 @@ function SEO({ description, lang, meta, title }) {
 }
 `}</script>
     </Helmet>
-  )
+  );
 }
 
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
-  description: ``,
-}
+  description: ``
+};
 
 SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string,
-}
+  title: PropTypes.string
+};
 
-export default SEO
+export default SEO;
