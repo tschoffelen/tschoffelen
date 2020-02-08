88513805a81cfce7be5f6784962ab21317ccb412
Thomas Schoffelen <thomas@schof.co>
2020-02-08 23:58:16 +0000

    Add 404 page.

---
+++ src/pages/404.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/Layout";
+import SEO from "../components/SEO";
+import Header from "../components/Header";
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="Page not found"/>
+
+    <Header/>
+
+    <h2>You're drunk.</h2>
+    <p>
+      Four-oh-four. Nothing here pal. <br/>
+      <Link className="link" to="/">Let me call you a cab.</Link>
+    </p>
+  </Layout>
+);
+
+export default NotFoundPage;
