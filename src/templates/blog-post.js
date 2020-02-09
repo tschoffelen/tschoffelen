d7f964199d2a2f406196fc2db32bf520dd0ee152
Thomas Schoffelen <thomas@schof.co>
2020-02-09 00:13:02 +0000

    Fix date format.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -40,7 +40,7 @@ export const query = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "MMMM D, YYYY")
       }
     }
   }
