b522f97d455931b120805947a2f522512bd9a40c
Thomas Schoffelen <thomas@schof.co>
2020-02-09 00:12:35 +0000

    Add RSS feed.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Thomas Schoffelen`,
     description: `Thomas Schoffelen is a tech entrepreneur and consultant, co-founder of NearSt and Infowijs, building tools to help small businesses and educators.`,
     author: `@tschoffelen`,
-    siteUrl: `https://schof.co/`,
+    siteUrl: `https://schof.co/`
   },
   plugins: [
     `gatsby-plugin-sass`,
@@ -24,27 +24,101 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#0c0d0e`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`,
-      },
+        icon: `src/images/icon.png`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./src/data/`,
-      },
+        path: `./src/data/`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./content/`,
-      },
+        path: `./content/`
+      }
     },
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
         siteUrl: `https://schof.co`,
-        stripQueryString: true,
-      },
+        stripQueryString: true
+      }
     },
-  ],
-}
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-99611142-1`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark, allPostsJson } }) => ([
+              ...allMarkdownRemark.edges.map(edge => ({
+                ...edge.node.frontmatter,
+                description: edge.node.excerpt,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + "/posts" + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + "/posts" + edge.node.fields.slug,
+                custom_elements: [{ "content:encoded": edge.node.html }]
+              })),
+              ...allPostsJson.nodes.map(edge => ({
+                title: edge.title,
+                description: edge.title,
+                date: edge.createdAt,
+                url: edge.link,
+                guid: edge.link
+              }))
+            ]),
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+                allPostsJson (
+                  limit: 6
+                ) {
+                  nodes {
+                    link
+                    title
+                    createdAt
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Thomas Schoffelen"
+          }
+        ]
+      }
+    }
+  ]
+};
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,33 +1,21 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 
-import "../styles/index.scss"
-
-const GA_TRACKING_ID = "UA-99611142-1"
+import "../styles/index.scss";
 
 const Layout = ({ children, className }) => (
   <main className={className}>
     {children}
-
-    <script
-      async
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-    />
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}');`,
-      }}
-    />
   </main>
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string,
-}
+  className: PropTypes.string
+};
 
 Layout.defaultProps = {
-  className: "default",
-}
+  className: "default"
+};
 
-export default Layout
+export default Layout;
