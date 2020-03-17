2f3d4a7312295b2ee9fcd335a6354d3ace2c83e2
Thomas Schoffelen <thomas@schof.co>
2020-03-17 09:55:44 +0000

    Code style fixes.

--- src/pages/meet.js
+++ src/pages/meet.js
@@ -7,24 +7,28 @@ import Header from "../components/Header"
 let loaded = false
 
 const MeetPage = () => {
-  if(!loaded && typeof window !== `undefined`){
-    var script = window.document.createElement('script');
-    script.src = 'https://assets.calendly.com/assets/external/widget.js';
-    window.document.head.appendChild(script);
-    loaded = true;
+  if (!loaded && typeof window !== `undefined`) {
+    var script = window.document.createElement("script")
+    script.src = "https://assets.calendly.com/assets/external/widget.js"
+    window.document.head.appendChild(script)
+    loaded = true
   }
 
   return (
-  <Layout>
-    <SEO title="Schedule a meeting" />
+    <Layout>
+      <SEO title="Schedule a meeting" />
 
-    <Header />
+      <Header />
 
-    <div className="calendly-inline-widget" data-url="https://calendly.com/tschof/30min?background_color=faf8f9&primary_color=355fc5" style={{minWidth: 300, height: 658}}/>
-    <style>{`main { max-width: none; }`}</style>
-    <script src="https://assets.calendly.com/assets/external/widget.js" />
-  </Layout>
-)
+      <div
+        className="calendly-inline-widget"
+        data-url="https://calendly.com/tschof/30min?background_color=faf8f9&primary_color=355fc5"
+        style={{ minWidth: 300, height: 658 }}
+      />
+      <style>{`main { max-width: none; }`}</style>
+      <script src="https://assets.calendly.com/assets/external/widget.js" />
+    </Layout>
+  )
 }
 
 export default MeetPage
