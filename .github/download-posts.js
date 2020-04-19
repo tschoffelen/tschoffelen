474739756df0866e4db6605ddeea609622f3ba3c
Thomas Schoffelen <thomas@schof.co>
2020-04-19 10:02:05 +0100

    Fix medium feedd

--- .github/download-posts.js
+++ .github/download-posts.js
@@ -7,12 +7,14 @@ axios
     "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40tschoffelen"
   )
   .then(res => {
-    data = res.data.items.map(({ title, pubDate, link, guid }) => ({
-      id: guid,
-      title: title.replace("&amp;", "&"),
-      link,
-      createdAt: moment(pubDate).toISOString(),
-    }))
+    data = res.data.items
+      .filter(({categories}) => categories.length)
+      .map(({ title, pubDate, link, guid }) => ({
+        id: guid,
+        title: title.replace("&amp;", "&"),
+        link,
+        createdAt: moment(pubDate).toISOString(),
+      }))
 
     fs.writeFileSync("./src/data/posts.json", JSON.stringify(data), "utf8")
     fs.writeFileSync("./static/posts.json", JSON.stringify(data), "utf8")
