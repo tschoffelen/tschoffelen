b1cbf3317cd978a419ad38a316b42a8dd8d482f9
Thomas Schoffelen <thomas@schof.co>
2021-03-24 18:04:12 +0000

    Cleanup.

--- src/functions/box.js
+++ src/functions/box.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3({ useAccelerateEndpoint: true });
 
-const illegalRe = /[\/\?<>\\:*|"]/g;
+const illegalRe = /[\/?<>\\:*|"]/g;
 const controlRe = /[\x00-\x1f\x80-\x9f]/g;
 const reservedRe = /^\.+$/;
 const windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
