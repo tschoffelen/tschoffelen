0876d291a9fd8e828994d2ade2e4242e822fe89c
Thomas Schoffelen <thomas@schof.co>
2021-03-24 19:11:04 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = ({
       </p>
       <p>
         In my spare time I write about life, friendships, engineering and
-        design.
+        user experience design.
       </p>
 
       <div className="links">
