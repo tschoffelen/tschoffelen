de873790ea13d745ca73d528f40db49d13c601dd
Thomas Schoffelen <thomas@schof.co>
2021-03-27 15:31:04 +0000

    Added consulting link.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,116 +1,130 @@
-import React from "react"
-import { graphql, Link } from "gatsby"
+import React from "react";
+import { graphql, Link } from "gatsby";
 
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
-import { organizePosts, renderPost } from "../utils"
+import Layout from "../components/Layout";
+import SEO from "../components/SEO";
+import { organizePosts, renderPost } from "../utils";
 
 const IndexPage = ({
   data: {
     allPostsJson: { nodes: mediumPosts },
     allMarkdownRemark: { nodes: blogPosts },
   },
-}) => (
-  <Layout className="homepage h-card">
-    <SEO />
+}) => {
+  const calendlyUrl = "https://calendly.com/tschof/consulting?primary_color=355fc5";
 
-    <div className="avatar">
-      <div className="avatar-inner" />
-    </div>
+  return (
+    <Layout className="homepage h-card">
+      <SEO />
 
-    <div>
-      <h2 className="p-name">Hi, I'm Thomas.</h2>
-
-      <p className="p-note">
-        I'm a co-founder of several tech startups, building tools to help small
-        businesses and educators. I love consulting founders and engineers on
-        everthing from starting their own company to designing complex
-        applications.
-      </p>
-      <p>
-        In my spare time I write about life, friendships, engineering and
-        user experience design.
-      </p>
-
-      <div className="links">
-        <h3>
-          <Link to="/posts">Recent posts</Link>
-        </h3>
-        {organizePosts([...mediumPosts, ...blogPosts])
-          .slice(0, 5)
-          .map(post => renderPost(post))}
+      <div className="avatar">
+        <div className="avatar-inner" />
       </div>
 
-      <div className="links">
-        <h3>What I'm working on</h3>
-        <a
-          href="http://near.st/?utm_source=schof.co"
-          className="p-org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          NearSt – making in-store inventory visible online
-        </a>
-        <a
-          href="https://streetartcities.com/?utm_source=schof.co"
-          className="p-org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Street Art Cities – collecting street art across the world
-        </a>
-        <a
-          href="http://infowijs.nl/?utm_source=schof.co"
-          className="p-org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Infowijs – allowing schools to communicate effectively
-        </a>
-      </div>
+      <div>
+        <h2 className="p-name">Hi, I'm Thomas.</h2>
 
-      <div className="links">
-        <h3>Find me online</h3>
-        <a
-          href="https://github.com/tschoffelen"
-          className="u-url"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          See my code on Github
-        </a>
-        <a
-          href="https://twitter.com/tschoffelen"
-          className="u-url"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Read my tweets
-        </a>
-        <a
-          href="https://instagram.com/tschoffelen"
-          className="u-url"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Follow me on Instagram
-        </a>
-        <a href="mailto:thomas@schof.co" className="u-email" rel="author">
-          Send me an email
-        </a>
-      </div>
+        <p className="p-note">
+          I'm a co-founder of several tech startups, building tools to help small
+          businesses and educators. I love consulting founders and engineers on
+          everthing from starting their own company to designing complex
+          applications.
+        </p>
+        <p>
+          In my spare time I write about life, friendships, engineering and
+          user experience design.
+        </p>
 
-      <a
-        style={{ display: "none" }}
-        aria-hidden="true"
-        href="https://schof.co"
-        className="u-url u-uid"
-      >
-        Thomas Schoffelen
-      </a>
-    </div>
-  </Layout>
-)
+        <div className="links">
+          <h3>
+            <Link to="/posts">Recent posts</Link>
+          </h3>
+          {organizePosts([...mediumPosts, ...blogPosts])
+            .slice(0, 5)
+            .map(post => renderPost(post))}
+        </div>
+
+        <div className="links">
+          <h3>What I'm working on</h3>
+          <a
+            href="http://near.st/?utm_source=schof.co"
+            className="p-org"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            NearSt – making in-store inventory visible online
+          </a>
+          <a
+            href="https://streetartcities.com/?utm_source=schof.co"
+            className="p-org"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Street Art Cities – collecting street art across the world
+          </a>
+          <a
+            href="http://infowijs.nl/?utm_source=schof.co"
+            className="p-org"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Infowijs – allowing schools to communicate effectively
+          </a>
+        </div>
+
+        <div className="links">
+          <h3>Get in touch</h3>
+          <a
+            href={calendlyUrl}
+            className="u-url"
+            rel="noopener noreferrer"
+            target="_blank"
+            onClick={(e) => {
+              if (typeof window !== "undefined") {
+                e.preventDefault && e.preventDefault();
+                window.Calendly.initPopupWidget({ url: calendlyUrl });
+                return false;
+              }
+            }}
+          >
+            Schedule a consulting session
+          </a>
+          <a
+            href="https://github.com/tschoffelen"
+            className="u-url"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            See my code on Github
+          </a>
+          <a
+            href="https://twitter.com/tschoffelen"
+            className="u-url"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Read my tweets
+          </a>
+          <a href="mailto:thomas@schof.co" className="u-email" rel="author">
+            Send me an email
+          </a>
+        </div>
+
+        <a
+          style={{ display: "none" }}
+          aria-hidden="true"
+          href="https://schof.co"
+          className="u-url u-uid"
+        >
+          Thomas Schoffelen
+        </a>
+
+        <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
+        <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async />
+      </div>
+    </Layout>
+  );
+};
 
 export const query = graphql`
   query posts {
@@ -138,6 +152,6 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default IndexPage
+export default IndexPage;
