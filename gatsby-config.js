86bad3670b8c90094745dc3c3bbd89cc8037830e
Thomas Schoffelen <thomas@schof.co>
2021-09-30 07:53:14 +0200

    Set correct theme colour, clean up obsolete 404.

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
         short_name: "Thomas",
         start_url: "/",
         background_color: "#ffffff",
-        theme_color: "#212733",
+        theme_color: "#f4f4f5",
         display: "minimal-ui",
         icon: "src/assets/icon.svg",
       },
