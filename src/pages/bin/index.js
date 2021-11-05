b8e77fbc9a4e604912fdd1614f2fb26f5ba2178c
Thomas Schoffelen <thomas@schof.co>
2021-11-05 17:51:35 +0000

    Add a /bin page rather than a redirect.

--- src/pages/bin/index.js
+++ src/pages/bin/index.js
@@ -1,16 +1,24 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { nanoid } from "nanoid";
 
+import "./style.scss";
+
 const BinPage = () => {
-  const binId = nanoid(6);
+  const createBin = () => {
+    const binId = nanoid(6);
+    window.location.href = `/bin/${binId}?edit=1`;
+  };
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.location.href = `/bin/${binId}?edit=1`;
-    }
-  }, []);
-
-  return <p>Creating a new bin...</p>;
+  return (
+    <div className="bin-home">
+      <p>
+        Bins allow you to store a set of images and show them on one web page.
+        <br />
+        Each bin has its own unique link you can share with others.
+      </p>
+      <button onClick={createBin}>Create a bin</button>
+    </div>
+  );
 };
 
 export default BinPage;
