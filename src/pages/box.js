961651cf6f7873bc832fa61d99f0e02e4da855af
Thomas Schoffelen <thomas@schof.co>
2022-01-07 20:38:42 +0000

    Fix box redirect.

--- src/pages/box.js
+++ src/pages/box.js
@@ -7,7 +7,15 @@ const BoxPage = () => {
     window.location.href = "https://schof.link/?utm_source=box";
   });
 
-  return <Layout>schof.link</Layout>;
+  return (
+    <Layout>
+      <p>schof.link</p>
+      <meta
+        httpEquiv="refresh"
+        content="0; url=https://schof.link/?utm_source=box"
+      />
+    </Layout>
+  );
 };
 
 export default BoxPage;
