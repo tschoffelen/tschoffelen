5bb844f8010fa33865a9d689d29459c69bd46b63
Thomas Schoffelen <thomas@schof.co>
2023-07-23 16:51:35 +0100

    feat: attempt to add syntax highlighting for more langs

--- components/blog/CodeHighlighter.js
+++ components/blog/CodeHighlighter.js
@@ -2,6 +2,8 @@
 
 import Prism from "prismjs";
 import { useEffect } from "react";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-yaml";
 
 export default function CodeHighlighter() {
   useEffect(() => {
