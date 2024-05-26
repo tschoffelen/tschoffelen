850ed359b21d1728c5e3a3ca15150a449746128e
Thomas Schoffelen <thomas@schof.co>
2024-05-26 12:57:45 +0100

    feat: add JSX syntax highlighting support

--- components/blog/CodeHighlighter.js
+++ components/blog/CodeHighlighter.js
@@ -4,6 +4,8 @@ import Prism from "prismjs";
 import { useEffect } from "react";
 import "prismjs/components/prism-bash";
 import "prismjs/components/prism-yaml";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-js-extras";
 
 export default function CodeHighlighter() {
   useEffect(() => {
