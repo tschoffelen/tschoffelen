421cedfb8629cb6464627c37d7ae7d55f7347e34
Thomas Schoffelen <thomas@schof.co>
2025-11-24 19:36:56 +0000

    chore: add cdk for domain redirects

---
+++ bin/cdk.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import * as patterns from "aws-cdk-lib/aws-route53-patterns";
+import * as route53 from "aws-cdk-lib/aws-route53";
+
+export class SchofDomainRedirectsCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // "schoffelen.net" domain redirect
+    new patterns.HttpsRedirect(this, "schoffelen.net-redirect", {
+      recordNames: ["schoffelen.net", "www.schoffelen.net", "thomas.schoffelen.net"],
+      targetDomain: "schof.co",
+      zone: route53.HostedZone.fromHostedZoneAttributes(this, "schoffelen.net-zone", {
+        hostedZoneId: "Z0628374GZPC2824PMPQ",
+        zoneName: "schoffelen.net",
+      }),
+    });
+
+    // "tschoffelen.com" domain redirect
+    new patterns.HttpsRedirect(this, "tschoffelen.com-redirect", {
+      recordNames: ["tschoffelen.com", "www.tschoffelen.com"],
+      targetDomain: "schof.co",
+      zone: route53.HostedZone.fromHostedZoneAttributes(this, "tschoffelen.com-zone", {
+        hostedZoneId: "Z04965401S0UMTWVDYKYY",
+        zoneName: "tschoffelen.com",
+      }),
+    });
+
+    // "thomasschoffelen.com" domain redirect
+    new patterns.HttpsRedirect(this, "thomasschoffelen.com-redirect", {
+      recordNames: ["thomasschoffelen.com", "www.thomasschoffelen.com"],
+      targetDomain: "schof.co",
+      zone: route53.HostedZone.fromHostedZoneAttributes(this, "thomasschoffelen.com-zone", {
+        hostedZoneId: "ZYO6Q7UJ88ZTJ",
+        zoneName: "thomasschoffelen.com",
+      }),
+    });
+  }
+}
+
+const app = new cdk.App();
+new SchofDomainRedirectsCdkStack(app, "schof-domain-redirects");
